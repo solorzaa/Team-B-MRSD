0a401d7b1ce44ef799f30bc10e2ece01db736f34
mehtajaghvi <jaghvi11@gmail.com>
2014-11-06 23:25:48 -0500

    added transmit file for surveygun

--- PROJECT FILES/Communication System/Arduino Tests/Radio3DR_TX/Radio3DR_TX.ino
+++ PROJECT FILES/Communication System/Arduino Tests/Radio3DR_TX/Radio3DR_TX.ino
@@ -23,7 +23,8 @@ void loop() {
 //      byteSent = 0;     
 //   }
 //   byteSent = byteSent + 1;
-   Serial.write(byteSent);
+Serial.write(byteSent);
+   Serial.print(byteSent);
    delay(1000);
    byteSent++;
 }
